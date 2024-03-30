01a87c25f71e07cef08795b3871ff4689d6fb1e4
Gift Mkwara <gift.mkwara@gmail.com>
2024-03-30 13:37:21 +1300

    Created the serial column

---
+++ src/test/kotlin/com/autodata/api/serials/SerialColumnTests.kt
@@ -0,0 +1,17 @@
+package com.autodata.api.serials
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+
+class SerialColumnTests :
+    DescribeSpec({
+        describe("When generating a value") {
+            it("Generates values sequentially") {
+                val values = mutableListOf<Int>()
+
+                repeat(100) { values.add(SerialColumn().generate()) }
+
+                values shouldBe (1..100).toList()
+            }
+        }
+    })
